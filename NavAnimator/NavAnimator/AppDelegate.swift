5eaacfd272ce309ec177c79af8289e51465a2e06
李晓萌 <bjlixiaomeng@corp.netease.com>
2016-05-22 21:40:27 +0800

    初始化项目

---
+++ NavAnimator/NavAnimator/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  NavAnimator
+//
+//  Created by 李晓萌 on 16/5/22.
+//  Copyright © 2016年 netease. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
